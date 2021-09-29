1a8f633a4dd4b169aadacbe464fa5f651f64d573
azizbek956 <azizbekshukurov11@gmail.com>
2021-09-29 22:02:20 +0500

    changed file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 var navTop = $("nav.navbar").offset().top;
 $(window).scroll(function () {
     var currentScroll = $(window).scrollTop();
-
     if ($(this).scrollTop()) {
         $("nav.navbar").removeClass("no-bg");
     } else {
         $("nav.navbar").addClass("no-bg");
     }
-
     if ($(this).scrollTop() > 300) {
         $(".back_to_top").addClass("show");
     } else {
         $(".back_to_top").removeClass("show");
     }
-
 });
+
 $(".back_to_top").click(function () { 
     $("html,body").animate({ scrollTop: 0 }, 0);
 });
-$("#contact_form").click(function () { 
-    $(".contact_form").slideToggle(800);
+// $("#message-logo").click(function () { 
+//     $(".contact_form").slideToggle(800);
+// });
+
+$(document).ready(function () {
+    $("#message-logo").click(function(){
+        $(".contact_form").fadeIn(650)
+    });
+    $(".sss").click(function(){
+        $(".contact_form").fadeOut(650)
+    });
 });
